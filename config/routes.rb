17824abf0c84349580954b2a472f79dd6a3dc7fd
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-08-13 13:44:00 -0500

    backend working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'cocktails#home'
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [ :new, :create ]
+  end
+  resources :doses, only: :destroy
 end
